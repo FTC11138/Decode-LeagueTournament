586f03a96ca7c20542e70a3eafe501dbd2dfc91a
Anish Agrawal <143356040+anish-agr@users.noreply.github.com>
2026-01-04 13:29:57 -0800

    feat: add blue autonomous routine and tune constants
    Co-authored-by: aarushkik <166559350+aarushkik@users.noreply.github.com>

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/subsystem/TurretStateCommand.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/subsystem/TurretStateCommand.java
@@ -3,12 +3,13 @@ package org.firstinspires.ftc.teamcode.commands.subsystem;
 import com.arcrobotics.ftclib.command.InstantCommand;
 
 import org.firstinspires.ftc.teamcode.hardware.Robot;
+import org.firstinspires.ftc.teamcode.hardware.subsystems.TurretOdometrySubsystem;
 import org.firstinspires.ftc.teamcode.hardware.subsystems.TurretSubsystem;
 
 public class TurretStateCommand extends InstantCommand {
-    public TurretStateCommand(TurretSubsystem.TurretState state) {
+    public TurretStateCommand(TurretOdometrySubsystem.TurretState state) {
         super(
-                () -> Robot.getInstance().turretSubsystem.setTurretState(state)
+                () -> Robot.getInstance().turretOdometrySubsystem.setTurretState(state)
         );
     }
 }
